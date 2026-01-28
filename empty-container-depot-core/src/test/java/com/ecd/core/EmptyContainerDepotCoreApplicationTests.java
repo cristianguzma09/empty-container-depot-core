c789dc1fdee341ca65226e778c1c03e16517fe94
Cristian Michael Guzman Jimenez <desarrollador27@tecsersas.com>
2026-01-28 12:07:10 -0500

    Initial commit: Spring Boot WAR structure (Java 17)

---
+++ empty-container-depot-core/src/test/java/com/ecd/core/EmptyContainerDepotCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecd.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmptyContainerDepotCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
