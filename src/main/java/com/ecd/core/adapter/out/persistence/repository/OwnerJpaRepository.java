d2cecb31e836c3252d221338a4fd7ae51db91707
Cristian Michael Guzman Jimenez <desarrollador27@tecsersas.com>
2026-01-30 10:39:51 -0500

    updating properties.yml to save owner entity through JPA Hibernate

--- src/main/java/com/ecd/core/adapter/out/persistence/repository/OwnerJpaRepository.java
+++ src/main/java/com/ecd/core/adapter/out/persistence/repository/OwnerJpaRepository.java
@@ -2,12 +2,23 @@ package com.ecd.core.adapter.out.persistence.repository;
 
 import com.ecd.core.adapter.out.persistence.entity.OwnerJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface OwnerJpaRepository extends JpaRepository<OwnerJpaEntity, Integer> {
+    @Query("""
+        select case when count(o) > 0 then true else false end
+        from OwnerJpaEntity o
+        where upper(o.ownerName) = upper(:ownerName)
+    """)
+    boolean existsByOwnerNameIgnoreCase(@Param("ownerName") String ownerName);
 
-    boolean existsByOwnerNameIgnoreCase(String ownerName);
-
-    boolean existsByOwnerCodeIgnoreCase(String ownerCode);
+    @Query("""
+        select case when count(o) > 0 then true else false end
+        from OwnerJpaEntity o
+        where upper(o.ownerCode) = upper(:ownerCode)
+    """)
+    boolean existsByOwnerCodeIgnoreCase(@Param("ownerCode") String ownerCode);
 }
 
 
